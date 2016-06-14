cefde4d2bd31ca90176b068c8ffb5be8301e59eb
Aaron Ding <aarond@amayasoftware.com>
2016-06-14 13:07:08 -0400

    print out map

--- maze.js
+++ maze.js
@@ -1,23 +1,24 @@
 'use strict';
 
-let mazeData = require('./mazeData.js'),
+var clc = require('cli-color'),
+  mazeData = require('./mazeData.js'),
   maze = mazeData.maze,
   mazeHeight = maze.length,
   mazeWidth = maze[0].length,
-  start = mazeData.start;
+  start = mazeData.start,
+  paths = {};
 
 var nextStep = (x, y) => {
   let available = false;
 
-  console.log(x, y, maze[y][x]);
-
   if (maze[y][x] === 0) {
+    paths[x + '-' + y] = { x: x, y: y};
+
     if (x === 0 || y === 0 || x === mazeWidth - 1 || y === mazeHeight - 1) {
-      console.log('found');
       return false;
     }
 
-    maze[y][x] = 1;
+    maze[y][x] = 2;
     if (nextStep(x + 1, y)) {
       available = true;
     } else if (nextStep(x, y + 1)) {
@@ -32,7 +33,36 @@ var nextStep = (x, y) => {
   return available;
 };
 
-debugger;
+console.time('step');
 nextStep(start.x, start.y);
+console.timeEnd('step');
 
-console.log('done');
+var map = '';
+for (let y = 0; y < mazeHeight; y++) {
+  let row = '';
+  for (let x = 0; x < mazeWidth; x++) {
+    let value = maze[y][x];
+    if (x === start.x && y === start.y) {
+      value = clc.red('S');
+    } else {
+      if (paths[x + '-' + y]) {
+        if (x === 0 || y === 0 || x === mazeWidth - 1 || y === mazeHeight - 1) {
+          value = clc.green('X');
+        } else {
+          value = clc.green('.');
+        }
+      } else {
+        if (value === 2 || value === 0) {
+          value = ' ';
+        } else {
+          value = '*';
+        }
+      }
+    }
+
+    row += value + ' ';
+  }
+  map += row + '\n';
+}
+
+console.log(map);
